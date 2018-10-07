21b23c46a1bc79546dcd563e6deacda5b35a0003
amebalibre <ameba@autistici.org>
2018-10-07 19:24:27 +0200

    Include short_content property for post model

--- portfolio/blog/models/post.py
+++ portfolio/blog/models/post.py
@@ -1,11 +1,12 @@
 """Models of Blog."""
-import datetime
-
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
 from .image import Image
 from .tag import Tag
+
+import alu_configparser as alu_cp
 
 
 class Post(models.Model):
@@ -17,11 +18,11 @@ class Post(models.Model):
         null=False,
         unique=True,
     )
-    content = models.CharField(max_length=1000)
+    content = models.TextField(max_length=1000)
     images = models.ManyToManyField(Image)
     pub_date = models.DateTimeField(
         'Date Published',
-        default=datetime.date.today,
+        auto_now_add=True,
     )
     tags = models.ManyToManyField(Tag)
 
@@ -32,6 +33,20 @@ class Post(models.Model):
     was_published.admin_order_field = 'pub_date'
     was_published.boolean = True
     was_published.short_description = 'Published recently?'
+
+    @property
+    def short_content(self):
+        """Return a fraction of the content."""
+        config = alu_cp.Configparser(
+            path=settings.ENVIRONMENT['development'],
+            fname=settings.FILECONFIG,
+        ).get()
+        _from = config.getint('portfolio.blog', 'short_content')
+        _to = self.content[:_from].rfind(' ')
+        return '{content}{excess}'.format(
+            content=self.content[:_to],
+            excess='...' if self.content[_from:] else ''
+        )
 
     def __str__(self):
         """Printable object."""
